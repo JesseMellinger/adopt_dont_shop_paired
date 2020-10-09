4ec9ec50443456c8112e2fc2ea17e3d0ad25a34b
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-10-08 18:30:46 -0600

    Create test for show view of Pets controller

---
+++ spec/features/pets/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "as a visitor" do
+  describe "when I visit /pets/:id" do
+    it "then I see the pet with that id including the pets image, name, description, approximate_age, sex, and adoption status" do
+      pet_1 = Pet.create(image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
+                         name: "Bolt",
+                         approximate_age: 5,
+                         sex: "male",
+                         name_of_shelter: "Animal Control and Shelter",
+                         descriptiion: "White haired German Shepherd. Sounds like John Travolta.",
+                         adoption_status: NULL)
+
+      visit("/pets/#{pet_1.id}")
+
+      expect(page).to have_content(pet_1.image)
+      expect(page).to have_content(pet_1.name)
+      expect(page).to have_content(pet_1.description)
+      expect(page).to have_content(pet_1.approximate_age)
+      expect(page).to have_content(pet_1.sex)
+      expect(page).to have_content(pet_1.adoption_status)
+    end
+  end
+end
