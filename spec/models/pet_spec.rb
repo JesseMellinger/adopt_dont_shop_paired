9f5d9385386f1a31d0cfb60dfe23fb96e551226f
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-10-09 18:22:15 -0600

    Create model and database associations between pets and shelters

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe Artist, type: :model do
+describe Pet, type: :model do
   describe "validations" do
-    it { should validate_presence_of :name }
+    it { should belong_to :shelter }
   end
 end
--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Artist, type: :model do
+describe Shelter, type: :model do
   describe "relationships" do
     it { should have_many :pets}
   end
