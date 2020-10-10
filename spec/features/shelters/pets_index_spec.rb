190e52226de2f6a0460552f81efd84138f47ad96
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-10-09 18:49:03 -0600

    Update spec file for shelter pets index

--- spec/features/shelters/pets_index_spec.rb
+++ spec/features/shelters/pets_index_spec.rb
@@ -37,6 +37,51 @@ describe "as a visitor" do
                                   state: "CO",
                                   zip: "81611")
 
+      pet_1 = Pet.create!(image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
+                          name: "Bolt",
+                          approximate_age: 5,
+                          sex: "male",
+                          name_of_shelter: "Eagle County Animal Services",
+                          description: "White haired German Shepherd. Sounds like John Travolta.",
+                          adoption_status: "NULL",
+                          shelter_id: shelter_1.id)
+
+      pet_2 = Pet.create!(image: "https://en.wikipedia.org/wiki/Higgins_(dog)#/media/File:Higgins_the_Dog.jpg",
+                          name: "Higgins",
+                          approximate_age: 63,
+                          sex: "male",
+                          name_of_shelter: "Animal Control and Shelter",
+                          description: "Most people remember him as the original Benji.",
+                          adoption_status: "NULL",
+                          shelter_id: shelter_2.id)
+
+      pet_3 = Pet.create!(image: "https://en.wikipedia.org/wiki/Rin_Tin_Tin#/media/File:Rin_Tin_Tin_1929.JPG",
+                          name: "Rin Tin Tin",
+                          approximate_age: 5,
+                          sex: "male",
+                          name_of_shelter: "Eagle County Animal Services",
+                          description: "White haired German Shepherd. Sounds like John Travolta.",
+                          adoption_status: "NULL",
+                          shelter_id: shelter_1.id)
+
+      pet_4 = Pet.create!(image: "https://moviepaws.files.wordpress.com/2015/10/luhad-einstein.jpg",
+                          name: "Einstein",
+                          approximate_age: 50,
+                          sex: "male",
+                          name_of_shelter: "Eagle Valley Humane Society",
+                          description: "Dr. Emmett Brown\'s pet sheepdog.",
+                          adoption_status: "NULL",
+                          shelter_id: shelter_3.id)
+
+      pet_5 = Pet.create!(image: "https://en.wikipedia.org/wiki/Rin_Tin_Tin#/media/File:Rin_Tin_Tin_1929.JPG",
+                          name: "Fly",
+                          approximate_age: 25,
+                          sex: "female",
+                          name_of_shelter: "Leadville/Lake County Animal Shelter",
+                          description: "Border collie from Babe.",
+                          adoption_status: "NULL",
+                          shelter_id: shelter_4.id)
+
       visit("/shelters/#{shelter_1.id}/pets")
 
       expect(page).to have_content(pet_1.image)
