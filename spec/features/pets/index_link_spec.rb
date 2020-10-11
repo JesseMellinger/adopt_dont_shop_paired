125a24f013dad142def6670ee4b3725a0ef45d3d
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-10-11 17:34:26 -0600

    Add a link to pet index page on all pages

---
+++ spec/features/pets/index_link_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+describe "as a visitor" do
+  describe "when I visit any page on the site" do
+    it "then I see a link at the top of the page that takes me to the Pet Index" do
+      shelter_1 = Shelter.create!(name: "Eagle County Animal Services",
+                                 address: "1400 Fairgrounds Road",
+                                 city: "Eagle",
+                                 state: "CO",
+                                 zip: "81631")
+
+      pet_1 = Pet.create!(image: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg",
+                          name: "Bolt",
+                          approximate_age: 5,
+                          sex: "male",
+                          name_of_shelter: shelter_1.name,
+                          description: "White haired German Shepherd. Sounds like John Travolta.",
+                          adoption_status: "adoptable",
+                          shelter_id: shelter_1.id)
+
+      visit("/pets/#{pet_1.id}/edit")
+
+      expect(page).to have_link("Pet Index", :href=>"/pets")
+
+      visit("/pets/#{pet_1.id}")
+
+      expect(page).to have_link("Pet Index", :href=>"/pets")
+
+      visit("/shelters/#{shelter_1.id}/edit")
+
+      expect(page).to have_link("Pet Index", :href=>"/pets")
+
+      visit("/shelters")
+
+      expect(page).to have_link("Pet Index", :href=>"/pets")
+
+      visit("/shelters/#{shelter_1.id}/pets/new")
+
+      expect(page).to have_link("Pet Index", :href=>"/pets")
+
+      visit("/shelters/new")
+
+      expect(page).to have_link("Pet Index", :href=>"/pets")
+
+      visit("/shelters/#{shelter_1.id}/pets")
+
+      expect(page).to have_link("Pet Index", :href=>"/pets")
+
+      visit("/shelters/#{shelter_1.id}")
+
+      expect(page).to have_link("Pet Index", :href=>"/pets")
+    end
+  end
+end
