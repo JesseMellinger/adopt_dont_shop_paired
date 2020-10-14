5053054ee0a63c6a975b44768dde633b047d2240
Kiera Allen <kieraaallen@gmail.com>
2020-10-14 16:06:53 -0600

    Create test to display reviews for user

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -44,9 +44,40 @@ describe "as a visitor" do
         zip: "81650"
       )
 
+      shelter_1 = Shelter.create!(name: "Eagle County Animal Services",
+                                 address: "1400 Fairgrounds Road",
+                                 city: "Eagle",
+                                 state: "CO",
+                                 zip: "81631")
+
+      review_1 = Review.create!(title: "Friends don\'t lie",
+                                rating: "5",
+                                content: "Only the educated are free.",
+                                picture: "https://upload.wikimedia.org/wikipedia/commons/0/00/Epicteti_Enchiridion_Latinis_versibus_adumbratum_%28Oxford_1715%29_frontispiece.jpg",
+                                shelter_id: shelter_1.id,
+                                user_id: user_1.id)
+
+      review_3 = Review.create!(title: "Great food",
+                                rating: "3",
+                                content: "I thought this was a restaurant but I was wrong. The bone-shaped cookies were good though.",
+                                picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Ensaladang_Lato_%28Seaweed_Salad%29_-_Philippines_1.jpg/594px-Ensaladang_Lato_%28Seaweed_Salad%29_-_Philippines_1.jpg",
+                                shelter_id: shelter_2.id,
+                                user_id: user_1.id)
+
       visit("/users/#{user_1.id}")
 
-      
+        within "##{user_1.id}-#{review_1.id}" do
+          expect(page).to have_content(review_1.title)
+          expect(page).to have_content(review_1.rating)
+          expect(page).to have_content(review_1.content)
+        end
+
+        within "##{user_1.id}-#{review_3.id}" do
+          expect(page).to have_content(review_3.title)
+          expect(page).to have_content(review_3.rating)
+          expect(page).to have_content(review_3.content)
+        end
+
     end
   end
 end
