c4a15cb333b77ac9b43957565bcb65478185719d
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-10-15 14:18:14 -0600

    Create test for user that doesn't exist

--- spec/features/shelter_reviews/new_spec.rb
+++ spec/features/shelter_reviews/new_spec.rb
@@ -19,6 +19,12 @@ require 'rails_helper'
 # I see a flash message indicating that I need to fill in a title, rating, and content in order to submit a shelter review
 # And I'm returned to the new form to create a new review
 
+# As a visitor,
+# When I visit the new review page
+# And I enter the name of a User that doesn't exist in the database, but still try to submit the form
+# I see a flash message indicating that the User couldn't be found
+# And I'm returned to the new form to create a new review
+
 describe "as a visitor" do
   describe "when I'm on a new review page, I can complete a form" do
     it "then I submit a form and return to that shelters show page where I see my review" do
@@ -82,6 +88,37 @@ describe "as a visitor" do
 
         expect(current_path).to eq("/shelters/#{shelter_1.id}/reviews/new")
 
+      end
+      describe "when I enter the name of a user that doesn't exist in the database, but still try to submit the form" do
+        it "then I see a flash message indicating that the User couldn't be found" do
+          shelter_1 = Shelter.create!(name: "Eagle County Animal Services",
+                                     address: "1400 Fairgrounds Road",
+                                     city: "Eagle",
+                                     state: "CO",
+                                     zip: "81631")
+
+          user_1 = User.create!(name: "Testy",
+                               street_address: "221B Baker St.",
+                               city: "London",
+                               state: "CO",
+                               zip: "81650")
+
+
+          visit("/shelters/#{shelter_1.id}/reviews/new")
+
+          fill_in("title", with: "Friends don\'t lie")
+          fill_in("rating", with: "5")
+          fill_in("picture", with: "https://upload.wikimedia.org/wikipedia/en/3/33/Silicon_valley_title.png")
+          fill_in("content", with: "Only the educated are free.")
+          fill_in("name", with: "Legolas")
+
+          click_on("Create Review")
+
+          expect(page).to have_content("User could not be found")
+
+          expect(current_path).to eq("/shelters/#{shelter_1.id}/reviews/new")
+
+        end
       end
     end
   end
