f496311dad2fd208aecea6f3f4441ddda68eb9c0
Kiera Allen <kieraaallen@gmail.com>
2020-10-14 20:12:45 -0600

    Create test for deleting a shelter's review

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -116,5 +116,45 @@ describe "as a visitor" do
 
       expect(current_path).to eq("/shelters/#{shelter_1.id}/#{review_2.id}/edit")
     end
+    it "then I see a link to delete the shelter review next to each review and I can delete a review" do
+      shelter_1 = Shelter.create!(name: "Eagle County Animal Services",
+                                 address: "1400 Fairgrounds Road",
+                                 city: "Eagle",
+                                 state: "CO",
+                                 zip: "81631")
+
+      user_1 = User.create!(name: "Testy",
+                            street_address: "221B Baker St.",
+                            city: "London",
+                            state: "CO",
+                            zip: "81650")
+
+      review_1 = Review.create!(title: "Friends don\'t lie",
+                                rating: "5",
+                                content: "Only the educated are free.",
+                                picture: "https://upload.wikimedia.org/wikipedia/commons/0/00/Epicteti_Enchiridion_Latinis_versibus_adumbratum_%28Oxford_1715%29_frontispiece.jpg",
+                                shelter_id: shelter_1.id,
+                                user_id: user_1.id)
+
+      review_2 = Review.create!(title: "Ohh yea, you gotta get schwifty.",
+                                rating: "4",
+                                content: "Hello, IT. Have you tried turning it off and on again?",
+                                picture: "https://upload.wikimedia.org/wikipedia/en/3/33/Silicon_valley_title.png",
+                                shelter_id: shelter_1.id,
+                                user_id: user_1.id)
+
+      visit("/shelters/#{shelter_1.id}")
+
+        within "#review-#{review_1.id}" do
+          page.find_link("Delete Review")
+        end
+
+        within "#review-#{review_2.id}" do
+          click_link "Delete Review"
+        end
+
+      expect(path).to eq("/shelters/#{shelter_1.id}")
+      expect(page).to have_no_content(review_2)
+    end
   end
 end
