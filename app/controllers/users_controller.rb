20e157dccdfedf3b7f6942ad93ff643c9e5d9fff
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-10-16 15:40:23 -0600

    Create instance variablein users controller to get average review rating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reviews = Review.where(user_id: params[:id])
+    @average_review_rating = @user.average_review_rating.to_f.round(1)
   end
 
   def new
