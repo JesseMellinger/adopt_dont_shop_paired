c9f755c77d2f40914af9bdc1da2b44f2ae8ad822
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-10-16 14:39:14 -0600

    Change rating column to type integer in reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_030902) do
+ActiveRecord::Schema.define(version: 2020_10_16_202845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2020_10_13_030902) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
-    t.string "rating"
+    t.integer "rating"
     t.string "content"
     t.string "picture"
     t.bigint "shelter_id"
--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -61,14 +61,14 @@ describe "as a visitor" do
                                   zip: "80443")
 
       review_1 = Review.create!(title: "Friends don\'t lie",
-                                rating: "5",
+                                rating: 5,
                                 content: "Only the educated are free.",
                                 picture: "https://upload.wikimedia.org/wikipedia/commons/0/00/Epicteti_Enchiridion_Latinis_versibus_adumbratum_%28Oxford_1715%29_frontispiece.jpg",
                                 shelter_id: shelter_1.id,
                                 user_id: user_1.id)
 
       review_3 = Review.create!(title: "Great food",
-                                rating: "3",
+                                rating: 3,
                                 content: "I thought this was a restaurant but I was wrong. The bone-shaped cookies were good though.",
                                 picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Ensaladang_Lato_%28Seaweed_Salad%29_-_Philippines_1.jpg/594px-Ensaladang_Lato_%28Seaweed_Salad%29_-_Philippines_1.jpg",
                                 shelter_id: shelter_2.id,
@@ -111,14 +111,14 @@ describe "as a visitor" do
                                   zip: "80443")
 
       review_1 = Review.create!(title: "Friends don\'t lie",
-                                rating: "5",
+                                rating: 5,
                                 content: "Only the educated are free.",
                                 picture: "https://upload.wikimedia.org/wikipedia/commons/0/00/Epicteti_Enchiridion_Latinis_versibus_adumbratum_%28Oxford_1715%29_frontispiece.jpg",
                                 shelter_id: shelter_1.id,
                                 user_id: user_1.id)
 
       review_3 = Review.create!(title: "Great food",
-                                rating: "3",
+                                rating: 3,
                                 content: "I thought this was a restaurant but I was wrong. The bone-shaped cookies were good though.",
                                 picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Ensaladang_Lato_%28Seaweed_Salad%29_-_Philippines_1.jpg/594px-Ensaladang_Lato_%28Seaweed_Salad%29_-_Philippines_1.jpg",
                                 shelter_id: shelter_2.id,
